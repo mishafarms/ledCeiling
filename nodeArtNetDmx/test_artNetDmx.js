c06232f71a5601089e0cffd1f9c8b765fbb02c71
mishafarms <git@mishafarms>
2014-08-28 15:06:07 -0700

    get an arg from the command line

--- nodeArtNetDmx/test_artNetDmx.js
+++ nodeArtNetDmx/test_artNetDmx.js
@@ -4,14 +4,23 @@
 var util = require("util");
 var ledCeiling = require('./ledCeiling.js');
 var getPixels = require("get-pixels");
-
+var filename;
 	
 	ledCeiling.setup();
 	
-	getPixels("c:\\Users\\mlw\\Downloads\\ifl-ceil.jpg", function(err, pixels) {
+	if( process.argv[2] === undefined )
+	{
+		filename = "c:\\Users\\mlw\\Downloads\\ifl-ceil.jpg";
+	}
+	else
+	{
+		filename = process.argv[2];
+	}
+
+	getPixels(filename, function(err, pixels) {
 		if(err)
 		{
-			console.log("Bad image path");
+			console.log("Bad image path " + filename);
 			return;
 		}
 		console.log("got pixels", pixels.shape.slice());
